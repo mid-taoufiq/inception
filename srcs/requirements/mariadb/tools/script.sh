288c835e878a9696207f6ee266f6381256577758
taoufiq ibariken <taoufiqibariken@gmail.com>
2026-06-09 12:20:32 -0400

    mariadb script finished, compose modif

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-chown -R mysql:mysql /val/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
+
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 if [ ! -d "/var/lib/mysql/mysql"]; then
     echo "creating mariadb database..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
-    mysql_safe &
-    pid=$!
+
+
     while !mysqladmin ping --silent; do
         sleep 1
     done
 
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${MYSQLROOTPASS}\`;"
-    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQLDB}\`;"
-    mysql -e "CREATE USER IF NOT EXISTS \`${MYSQLUSR}\`@'%' IDENTIFIED BY '${MYSQLUSEPASS}';"
-    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQLDB}\`.* TO \`${MYSQLUSR}\`@'%';"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQLROOTPASS}';
+    CREATE DATABASE IF NOT EXISTS \`${MYSQLDB}\`;
+    CREATE USER IF NOT EXISTS \`${MYSQLUSR}\`@'%' IDENTIFIED BY '${MYSQLUSEPASS}';
+    GRANT ALL PRIVILEGES ON \`${MYSQLDB}\`.* TO \`${MYSQLUSR}\`@'%';" | mysqld --user=mysql --bootstrap
 
-    kill $pid
-    wait $pid
-
-    echo "mariadb database created!"
-else
-    echo "mariadb database already exists!"
 fi
 
-echo "mariadb service started!"
-exec mysql_safe
+echo "starting mariadb!"
+exec mysqld --user=mysql --console
