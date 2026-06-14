df1849beacbe3514e2459c176cc3f0d8e0ce673e
taoufiq ibariken <taoufiqibariken@gmail.com>
2026-06-14 11:57:44 -0400

    ..

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo "entered mariadb script..."
+set -e
 
-chown -R mysql:mysql /var/lib/mysql
+echo "entered mariadb script..."
 
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-    echo "creating mariadb database..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
-
-    echo "FLUSH PRIVILEGES;
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';
-    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;
-    CREATE USER IF NOT EXISTS \`${MYSQL_USR}\`@'%' IDENTIFIED BY '${MYSQL_USR_PASS}';
-    GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USR}\`@'%';
-    FLUSH PRIVILEGES;" | mysqld --user=mysql --bootstrap
+    echo "mysql not installed, installing mysql now..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 fi
+
+echo "creating mariadb database..."
+
+echo "FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;
+CREATE USER IF NOT EXISTS \`${MYSQL_USR}\`@'%' IDENTIFIED BY '${MYSQL_USR_PASS}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USR}\`@'%';" | mysqld --user=mysql --bootstrap
 
 echo "starting mariadb database..."
 exec mysqld --user=mysql --console --port=${MARIADB_PORT}
