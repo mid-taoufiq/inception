84e708204e3329ad0833536f9d9eb799d0204373
taoufiq <taoufiqibariken@gmail.com>
2026-06-21 10:05:13 -0400

    ...

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-if [ ! id -u "${FTP_USR}" > /dev/null ]; ther
+if ! id -u "${FTP_USR}" 2> /dev/null; then
 
     useradd -d /var/www/html "${FTP_USR}"
     echo "${FTP_USR}:${FTP_PASS}" | chpasswd
     echo "${FTP_USR}" >> /etc/vsftpd.userlist
-    usermod -aG www-data "$FTP_USER"
+    usermod -aG www-data "${FTP_USR}"
 
 fi
 
