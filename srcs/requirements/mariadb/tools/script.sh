c215d65644fb95ea4497ce6a1b4fa1687b0c5b9e
taoufiq <taoufiqibariken@gmail.com>
2026-06-26 06:44:04 -0400

    ...

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "/\[mysqld\]/a port = ${MARIADB_PORT}" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 echo "FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';
